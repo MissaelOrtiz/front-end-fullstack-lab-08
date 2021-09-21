2d5587e148c748061905ea3cd481579c8f6898a8
Missael Ortiz <ortiz.missael80@gmail.com>
2021-09-21 10:08:25 -0700

    added router integration, navigation working

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,6 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import GamesList from '../games/GameList';
+import GameDetail from '../games/GameDetail';
 
 export default function App() {
-  return <GamesList />;
+  return (
+    <Router>
+      <header>
+        <nav>
+          <Link to="/"> Home </Link>
+        </nav>
+      </header>
+      <Switch>
+        <Route path="/:id">
+          <GameDetail />
+        </Route>
+        <Route path="/">
+          <GamesList />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
